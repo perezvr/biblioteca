6f4672d75abd17da0e8057c15aeff529e4e5ae87
Rodrigo Fernandes <digonalha@gmail.com>
2020-05-02 19:27:54 -0300

    trabalhando o insert de livros

--- frontend/src/pages/Main/index.js
+++ frontend/src/pages/Main/index.js
@@ -4,12 +4,34 @@ import { Container, Form, SubmitButton } from './style';
 
 export default class Main extends Component {
   state = { 
-    newBook: '',
-    books: [],
+    title: '',
+    author: '',
+    publisher: '',
+    genre: '',
     loading: false,
   }
 
+  handleInput = async (e) => {
+    const { value, name } = e.target;
+
+    this.setState({ [name]: value});
+  }
+
+  handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const { title, author, publisher, genre} = this.state;
+
+    console.log({
+      title,
+      author,
+      publisher,
+      genre
+    })
+  }
+
   render(){
+    const { title, author, publisher, genre} = this.state;
     return (
       <Container>
       <h1>
@@ -17,11 +39,11 @@ export default class Main extends Component {
       </h1>
       <Form onSubmit={this.handleSubmit}>
         <h1>Cadastrar Livro</h1>
-        <input type="text" placeholder="Título"/>
-        <input type="text" placeholder="Autor"/>
-        <input type="text" placeholder="Editora"/>
-        <input type="text" placeholder="Gênero"/>
-        <SubmitButton loadin={this.state.loading}>Adicionar</SubmitButton>
+        <input type="text" name="title" value={title} onChange={ this.handleInput }placeholder="Título"/>
+        <input type="text" name="author" value={author} onChange={ this.handleInput } placeholder="Autor"/>
+        <input type="text" name="publisher" value={publisher} onChange={ this.handleInput } placeholder="Editora"/>
+        <input type="text" name="genre" value={genre} onChange={ this.handleInput } placeholder="Gênero"/>
+        <SubmitButton loadin={this.state.loading} >Adicionar</SubmitButton>
 
       </Form>
       </Container>
